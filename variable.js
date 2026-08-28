898dee7023d8850a65b33aaf6b40c46e790b496f
Sumi Akter <sumiwdcse@gmail.com>
2026-08-28 12:25:04 +0600

    Explore more about Variables & JavaScript Data Types

--- variable.js
+++ variable.js
@@ -8,8 +8,12 @@ name = "Sumi";
 const age = 21;
 // age = 20;
 // console.log(age)
-
+const email = "sumi12@gmail.com";
 const isAdult = true;
 
 console.log(typeof(name), typeof(age), typeof(isAdult));
+console.log("typeof(name) = " + typeof(name));
+console.log("typeof(age) = " + typeof(age));
+console.log("typeof(email) = " + typeof(email));
+console.log("typeof(isAdult) = " + typeof(isAdult));
 console.log("If we declare a variable with let we can change the variable value but if we declare a variable with const we cannot change the variable value");
