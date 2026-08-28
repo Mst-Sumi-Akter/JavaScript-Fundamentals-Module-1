c5cd4fa76451b243a276a10041a6da0154885d50
Sumi Akter <sumiwdcse@gmail.com>
2026-08-28 12:18:53 +0600

    Explore Variables & JavaScript Data Types

---
+++ variable.js
@@ -0,0 +1,15 @@
+console.log("Learning variable js");
+
+let name = "Rahim";
+name = "Karim";
+name = "Sumi";
+ console.log(name);
+
+const age = 21;
+// age = 20;
+// console.log(age)
+
+const isAdult = true;
+
+console.log(typeof(name), typeof(age), typeof(isAdult));
+console.log("If we declare a variable with let we can change the variable value but if we declare a variable with const we cannot change the variable value");
